0dac14ad55b1b3c6596fadb3de5592acf638cd3f
Gervaiskouadio <lelagervais1@gmail.com>
2023-04-24 04:06:01 -0700

    README.md file & taske 0

---
+++ sort.h
@@ -0,0 +1,23 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+#include <deck.h>
+
+/* Sorting Algorithms for Arrays */
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
+/* Sorting Algorithm for Decks */
+void sort_deck(deck_node_t **deck);
+
+#endif /* SORT_H */
